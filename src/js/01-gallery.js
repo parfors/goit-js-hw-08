ec3b47380cedd3490c194d6753abc91a9267c2a3
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-05-29 22:42:54 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = obj => {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+</a>`,
+    ''
+  );
+};
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems));
+
+// new SimpleLightbox('.gallery a', {  captionDelay: 250,});
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  overlayOpacity: 0.6,
+  captionPosition: 'outside',
+  captionDelay: 250,
+});
 console.log(galleryItems);
