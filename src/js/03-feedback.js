62abf6ecd2770276216e235c38a91b83f859654a
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-05-30 21:15:08 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,38 @@
+import throttle from 'lodash.throttle';
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
-}
+  form: document.querySelector('.feedback-form'),
+};
 const inputData = {};
-refs.form.addEventListener('input', handlerInput);
-
+refs.form.addEventListener('input', throttle(handlerInput, 500));
+refs.form.addEventListener('submit', handlerSubmit);
 checkLocalStorage();
 
 function checkLocalStorage() {
-    // const storageData = JSON.parse('feedback-form-state');
-    // if (storageData) {
-    refs.form.email.value = '5';
-    
-        console.log(refs.form.email.value);
-    // }
+  const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (storageData) {
+    for (const key in storageData) {
+      refs.form[key].value = storageData[key];
+    }
+  }
 }
 
 function handlerInput(event) {
-    console.log(event.target.value);
-    inputData[event.target.name] = event.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(inputData))
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
 }
 
-
-
-
-
-// function checkLocalStorage() {
-//   const storageData = JSON.parse(localStorage.getItem(USER_INPUT_FORM_KEY));
-//   if (storageData) {
-//     for (key in storageData) {
-//       refs.formLinks[key].value = storageData[key];
-//     }
-//   }
-// }
-
-// function addToLocalStorage(e) {
-//   inputData[e.target.name] = e.target.value;
-//   localStorage.setItem(USER_INPUT_FORM_KEY, JSON.stringify(inputData));
-// }
+function handlerSubmit(event) {
+  event.preventDefault();
+  const isValid = [...event.target.elements]
+    .filter(el => el.nodeName === 'INPUT' || el.nodeName === 'TEXTAREA')
+    .every(el => el.value !== '');
+  console.log(isValid);
+  if (isValid) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.clear();
+    refs.form.reset();
+  } else {
+    alert('Fill in the fields');
+  }
+}
