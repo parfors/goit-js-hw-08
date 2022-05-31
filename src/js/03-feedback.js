a6a622deae83e5ac34f718b158338d2e688487b9
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-05-31 12:50:26 +0300

    fix local storage data recording

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,21 +3,29 @@ import throttle from 'lodash.throttle';
 const refs = {
   form: document.querySelector('.feedback-form'),
 };
-const inputData = {};
+
+let inputData = {};
+
 refs.form.addEventListener('input', throttle(handlerInput, 500));
 refs.form.addEventListener('submit', handlerSubmit);
 checkLocalStorage();
 
 function checkLocalStorage() {
+    const { email, message } = refs.form;
   const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (storageData) {
-    for (const key in storageData) {
-      refs.form[key].value = storageData[key];
+      for (const key in storageData) {
+    //   email.value = storageData.email || "";
+    //   message.value = storageData.message || "";
+      refs.form[key].value = storageData[key] || "";
     }
   }
 }
 
 function handlerInput(event) {
+    const storageData = localStorage.getItem('feedback-form-state', JSON.stringify(inputData));
+    console.log(storageData);
+    inputData = storageData === null ? {} : JSON.parse(localStorage.getItem('feedback-form-state'));  
   inputData[event.target.name] = event.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
 }
